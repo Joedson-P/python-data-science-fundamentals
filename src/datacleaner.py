83b867e8f1e29f5a85f0ca1edbec3c281cbd5dbf
Joedson Pereira <joedson.pereira96@gmail.com>
2025-10-22 18:49:27 -0300

    feat(atividade3): Implementa método pipeline() e decoradores (@classmethod/@staticmethod) para arquitetura de API.

--- src/datacleaner.py
+++ src/datacleaner.py
@@ -24,3 +24,30 @@ class DataCleaner:
             self.df['preco_unitario'], self.df['quantidade_vendida']
             )]
         return self.df
+    
+    def pipeline(self):
+        '''
+        Executa o pipeline completo de limpeza e transformação de dados
+        '''
+        self.calcular_faturamento()
+        print("— Faturamento calculado.")
+        return self.df
+    
+    @classmethod
+    def from_csv(cls, caminho_arquivo: str, *args, **kwargs):
+        '''
+        Cria uma instância de DataCleaner a partir de um arquivo CSV
+        '''
+        #Utilizando o pop() para extrair o encoding (caso exista). Se não existir, usa None ou 'utf-8' como padrão.
+        encoding_csv = kwargs.pop('encoding', 'utf-8')
+
+        # repassa apenas o 'encoding' para o pd.read_csv
+        df = pd.read_csv(caminho_arquivo, encoding = encoding_csv)
+
+        # repassa os outros args e kwargs para que o __init__ possa utilizá-los
+        return cls(df, *args, **kwargs)
+    
+    @staticmethod
+    def converter_para_milhares(valor):
+        return round(valor/1000, 3) # converte para milhares com 3 casas decimais
+    
